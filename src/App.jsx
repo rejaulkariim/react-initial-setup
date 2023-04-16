4f7598955b001f11f346a34d9fabef3d4c8652dd
rejaul-karim10 <rejalkarim66666@gmail.com>
2023-04-17 05:08:45 +0800

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./styles.css";
+export default function App() {
+  return (
+    <form className="new-item-form">
+      <div className="form-row">
+        <label htmlFor="item">New Item</label>
+        <input type="text" id="item" />
+      </div>
+    </form>
+  );
+}
